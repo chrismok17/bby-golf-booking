9ead43c85f66604af87f6cb301472d633fdca41b
Chris M <chrismok97@gmail.com>
2023-08-22 12:50:05 -0700

    allows the script to now select the earliest day

--- main.py
+++ main.py
@@ -3,51 +3,44 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import StaleElementReferenceException
+import time
 
 def input_details():
     current_day = int(input(f'What day is it today? (Enter a number): '))
     number_of_players = int(input(f'How many golfers? (Enter a number): '))
     return current_day, number_of_players
 
-def initiate_driver(current_day, num_players):
-    earliest_day = current_day + 5
-
+def initiate_driver():
     driver = webdriver.Chrome()
     driver.get("https://golfburnaby.cps.golf/onlineresweb/search-teetime?TeeOffTimeMin=0&TeeOffTimeMax=23")
-    driver.implicitly_wait(30.0)
+    
+    driver.maximize_window()
+    
+    return driver
 
-    wait = WebDriverWait(driver, 30)
-
-    form_element = wait.until(EC.element_to_be_clickable((By.XPATH, f'/html/body/app-root/app-full-layout/div/mat-sidenav-container/mat-sidenav-content/div[1]/app-search-teetime-page/div/div[1]/app-search-teetime-filters/div/div/div[3]/div[2]/mat-form-field/div/div[1]/div[2]/span/label/mat-label'))) 
-    driver.execute_script("arguments[0].click();", form_element)
-
-
-    # NOT WORKING YET YAHOO
-    for _ in range(35):
+def set_options(current_day, num_players, driver):
+    wait = WebDriverWait(driver, 60)
+    earliest_day = current_day + 5
+    day_elements = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "day-unit")))
+    for day_element in day_elements:
+        day_number = int(day_element.text)
         try:
-            day_elements = driver.find_elements(By.XPATH, "//span[contains(@class, 'day-unit')]")
-            for day in day_elements:
+            if day_number == earliest_day:
                 try:
-                    day_value = day.find_element(By.CLASS_NAME, "day-background-upper")
-                    if day_value.text == earliest_day:
-                        driver.execute_script("arguments[0].click();", day_value)
-                        break
+                # Click on the day element
+                    print("Clicking on day:", day_number)
+                    driver.execute_script("arguments[0].click()", day_element)
+                    break
                 except StaleElementReferenceException:
+                    print("Retrying")
                     continue
         except StaleElementReferenceException:
+            print("some shit")
             continue
 
-    # day_elements = driver.find_elements(By.XPATH, "//span[contains(@class, 'day-unit')]")
-    # for day in day_elements:
-    #     try:
-    #         day_value = day.find_element(By.CLASS_NAME, "day-background-upper")
-    #         print(day_value.text)
-    #         if day_value.text == earliest_day:
-    #             driver.execute_script("arguments[0].click();", day_value)
-    #             break
-    #     except StaleElementReferenceException:
-    #         # Handle the stale element reference exception here, e.g., re-run the loop or refresh the page
-    #         continue
+    # For mobile view
+    # form_element = wait.until(EC.element_to_be_clickable((By.XPATH, f'/html/body/app-root/app-full-layout/div/mat-sidenav-container/mat-sidenav-content/div[1]/app-search-teetime-page/div/div[1]/app-search-teetime-filters/div/div/div[3]/div[2]/mat-form-field/div/div[1]/div[2]/span/label/mat-label'))) 
+    # driver.execute_script("arguments[0].click();", form_element)
 
     input("Press Enter to close the browser window...")
 
@@ -55,7 +48,9 @@ def initiate_driver(current_day, num_players):
 
 def main():
     current_day, number_of_players = input_details()
-    initiate_driver(current_day, number_of_players)
+    driver = initiate_driver()
+    time.sleep(2)
+    set_options(current_day, number_of_players, driver)
 
 if __name__ == "__main__":
     main()
